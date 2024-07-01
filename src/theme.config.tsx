e58122da10d2307650af37ea12b8725febc30e87
tuntun0609 <760523867@qq.com>
2024-07-01 21:59:07 +0800

    feat: init

---
+++ src/theme.config.tsx
@@ -0,0 +1,10 @@
+import { type DocsThemeConfig, useConfig } from 'nextra-theme-docs'
+
+const themeConfig: DocsThemeConfig = {
+  logo: <span>My Nextra Documentation</span>,
+  project: {
+    link: 'https://github.com/shuding/nextra',
+  },
+}
+
+export default themeConfig
